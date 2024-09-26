9059974bb7d0be3a3befd624aa9e5e42620deb10
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-26 14:38:27 +0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 const app = express();
@@ -6,6 +7,55 @@ const port = process.env.PORT || 5000;
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+// user-management-system
+// mHQiMeTDpHZrMyMB
+
+
+
+const uri = "mongodb+srv://crud:ivSqEEul4nlc3kp7@cluster0.ejjfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db('User_Management');
+    const userCollection = database.collection('Users');
+
+    app.post('/users', async (req,res) => {
+      const user = req.body;
+      console.log("user", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('User Management System Project')
