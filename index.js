3428225b98f7cdd7d0c8bdad2a05b822fce0c8f3
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-26 15:45:40 +0600

    delete update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 const app = express();
@@ -45,6 +45,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('Please delete from database', id);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query); 
+      res.send(result);  
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
